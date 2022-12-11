c31484ffdbb44bcf8365099e2b53883a4a9f8f18
Vova Pol <ya-chai777@yandex.ru>
2022-12-11 18:40:20 +0300

    feat: сделал верстку линка в хедере

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
-function Header() {
+import { Link } from "react-router-dom";
+
+function Header(props) {
   return (
     <header className="header">
-      <div className="header__logo"></div>
+      <div className="header__logo-container">
+        <div className="header__logo"></div>
+        <Link className="header__link" to="/sign-in">
+          Привет
+        </Link>
+      </div>
       <hr className="header__line" />
     </header>
   );
