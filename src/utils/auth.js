54fe6f8419ac8ec922ec5a3fb584d25d041d56c0
Vova Pol <ya-chai777@yandex.ru>
2022-12-14 13:33:16 +0300

    refactor: переписал методы регистрации и авторизации с использованием класса Api.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,38 +1,47 @@
+import { Api } from "./api";
+
+const authConfig = {
+  baseUrl: "https://auth.nomoreparties.co/",
+};
+
+const apiAuth = new Api(authConfig);
 export const BASE_URL = "https://auth.nomoreparties.co";
 
 export function register(registerData) {
-  return fetch(`${BASE_URL}/signup`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(registerData),
-  }).then((res) => {
-    console.log(JSON.stringify(registerData));
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject("Сервер ответил ошибкой: " + res.status);
-    }
-  });
+  // return fetch(`${BASE_URL}/signup`, {
+  //   method: "POST",
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(registerData),
+  // }).then((res) => {
+  //   console.log(JSON.stringify(registerData));
+  //   if (res.ok) {
+  //     return res.json();
+  //   } else {
+  //     return Promise.reject("Сервер ответил ошибкой: " + res.status);
+  //   }
+  // });
+  return apiAuth.sendRequest("signup", "POST", registerData);
 }
 
 export function authorize(authorizationData) {
-  return fetch(`${BASE_URL}/signin`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(authorizationData),
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject("Сервер ответил ошибкой: " + res.status);
-    }
-  });
+  // return fetch(`${BASE_URL}/signin`, {
+  //   method: "POST",
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(authorizationData),
+  // }).then((res) => {
+  //   if (res.ok) {
+  //     return res.json();
+  //   } else {
+  //     return Promise.reject("Сервер ответил ошибкой: " + res.status);
+  //   }
+  // });
+  return apiAuth.sendRequest("signin", "POST", authorizationData);
 }
 
 export function checkToken(token) {
