1b1e8f3b8ac4489c6184c7f3c8f7d341e1a31f66
Vova Pol <ya-chai777@yandex.ru>
2022-12-14 17:54:41 +0300

    refactor: переписал функцию проверки токена пользователя

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,57 +5,20 @@ const authConfig = {
 };
 
 const apiAuth = new Api(authConfig);
-export const BASE_URL = "https://auth.nomoreparties.co";
 
 export function register(registerData) {
-  // return fetch(`${BASE_URL}/signup`, {
-  //   method: "POST",
-  //   headers: {
-  //     Accept: "application/json",
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify(registerData),
-  // }).then((res) => {
-  //   console.log(JSON.stringify(registerData));
-  //   if (res.ok) {
-  //     return res.json();
-  //   } else {
-  //     return Promise.reject("Сервер ответил ошибкой: " + res.status);
-  //   }
-  // });
   return apiAuth.sendRequest("signup", "POST", registerData);
 }
 
 export function authorize(authorizationData) {
-  // return fetch(`${BASE_URL}/signin`, {
-  //   method: "POST",
-  //   headers: {
-  //     Accept: "application/json",
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify(authorizationData),
-  // }).then((res) => {
-  //   if (res.ok) {
-  //     return res.json();
-  //   } else {
-  //     return Promise.reject("Сервер ответил ошибкой: " + res.status);
-  //   }
-  // });
   return apiAuth.sendRequest("signin", "POST", authorizationData);
 }
 
 export function checkToken(token) {
-  return fetch(`${BASE_URL}/users/me`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject("Сервер ответил ошибкой: " + res.status);
-    }
-  });
+  const checkTokenApiConfig = {
+    baseUrl: "https://auth.nomoreparties.co/",
+    token: `Bearer ${token}`,
+  };
+  const checkTokenApi = new Api(checkTokenApiConfig);
+  return checkTokenApi.sendRequest("users/me", "GET");
 }
