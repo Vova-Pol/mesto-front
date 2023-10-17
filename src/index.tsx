ef02e7b9b7d1c68bd8910651cae1681f3867c3df
Vova Pol <ya-chai777@yandex.ru>
2023-10-17 11:06:09 +0300

    Переименованы файлы компонентов в .tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App.js';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <BrowserRouter>
     <React.StrictMode>
