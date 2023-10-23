0912daa97de595d89c3cf5f320cbc045dadc0563
Vova Pol <ya-chai777@yandex.ru>
2023-10-23 11:35:14 +0300

    Компонент Card перенесен на typescript

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,49 +1,60 @@
-import React from "react";
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import React, { ReactElement } from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import { ICard } from '../types/cards';
 
-function Card(props) {
+interface ICardProps {
+  cardData: ICard;
+  onCardClick: (cardData: ICard) => void;
+  onCardLike: (cardData: ICard) => void;
+  onCardDelete: (cardData: ICard) => void;
+}
+
+function Card({
+  cardData,
+  onCardClick,
+  onCardDelete,
+  onCardLike,
+}: ICardProps): ReactElement {
   const currentUser = React.useContext(CurrentUserContext);
 
-  const isOwn = props.cardData.owner._id === currentUser._id;
+  const isOwn = cardData.owner._id === currentUser._id;
   const cardDeleteButtonClassName = isOwn
-    ? "elements__delete-button"
-    : "elements__delete-button elements__delete-button_hidden";
+    ? 'elements__delete-button'
+    : 'elements__delete-button elements__delete-button_hidden';
 
-  const isLiked = props.cardData.likes.some(
-    (user) => user._id === currentUser._id
-  );
+  const isLiked = cardData.likes.some((user) => user._id === currentUser._id);
   const cardLikeButtonClassName = isLiked
-    ? "elements__like-button elements__like-button_active"
-    : "elements__like-button";
+    ? 'elements__like-button elements__like-button_active'
+    : 'elements__like-button';
 
   function handleClick() {
-    props.onCardClick(props.cardData);
+    onCardClick(cardData);
   }
 
   function handleLikeClick() {
-    props.onCardLike(props.cardData);
+    onCardLike(cardData);
   }
 
   function handleDeleteClick() {
-    props.onCardDelete(props.cardData);
+    onCardDelete(cardData);
   }
   return (
-    <li className="elements__item" key={props.cardData._id}>
+    <li className="elements__item" key={cardData._id}>
       <img
-        src={props.cardData.link}
-        alt={props.cardData.name}
+        src={cardData.link}
+        alt={cardData.name}
         className="elements__image"
         onClick={handleClick}
       />
       <div className="elements__info">
-        <h2 className="elements__title">{props.cardData.name}</h2>
+        <h2 className="elements__title">{cardData.name}</h2>
         <div className="elements__like-container">
           <button
             className={cardLikeButtonClassName}
             onClick={handleLikeClick}
           ></button>
           <span className="elements__like-counter">
-            {props.cardData.likes.length}
+            {cardData.likes.length}
           </span>
         </div>
       </div>
