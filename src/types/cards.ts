f88bf0e9edebed88253e02e38cd6aeaf5c9a83fa
Vova Pol <ya-chai777@yandex.ru>
2023-10-23 11:34:29 +0300

    Добавлен тип ICard

--- src/types/cards.ts
+++ src/types/cards.ts
@@ -1,4 +1,15 @@
+import { IUser } from './user';
+
 export interface IUpdateCardsValues {
   cardName: string;
   imageUrl: string;
+}
+
+export interface ICard {
+  likes: IUser[];
+  _id: string;
+  name: string;
+  link: string;
+  owner: IUser;
+  createdAt: string;
 }
