f8587036a92a8e1f6fcc571c85ac1a7d66df6384
Vova Pol <ya-chai777@yandex.ru>
2023-11-03 17:20:56 +0300

    Компонент InfoTooltip перенесен на ts

--- src/components/InfoTooltip.tsx
+++ src/components/InfoTooltip.tsx
@@ -1,27 +1,37 @@
-import React from "react";
+import React, { ReactElement } from 'react';
 
-function InfoTooltip(props) {
+interface IInfoTooltipProps {
+  isOpen: boolean;
+  isSuccess: boolean;
+  onClose: () => void;
+}
+
+function InfoTooltip({
+  isOpen,
+  isSuccess,
+  onClose,
+}: IInfoTooltipProps): ReactElement {
   return (
-    <div className={props.isOpen ? "popup popup_opened" : "popup"}>
+    <div className={isOpen ? 'popup popup_opened' : 'popup'}>
       <div className="tooltip">
         <span
           className={
-            props.isSuccess
-              ? "tooltip__icon tooltip__icon_type_success"
-              : "tooltip__icon tooltip__icon_type_error"
+            isSuccess
+              ? 'tooltip__icon tooltip__icon_type_success'
+              : 'tooltip__icon tooltip__icon_type_error'
           }
         ></span>
         <div className="tooltip__text-container">
           <p className="tooltip__text">
-            {props.isSuccess
-              ? "Вы успешно зарегистрировались!"
-              : "Что-то пошло не так! Попробуйте ещё раз."}
+            {isSuccess
+              ? 'Вы успешно зарегистрировались!'
+              : 'Что-то пошло не так! Попробуйте ещё раз.'}
           </p>
         </div>
         <button
           className="popup__close-button"
           type="button"
-          onClick={props.onClose}
+          onClick={onClose}
         ></button>
       </div>
     </div>
